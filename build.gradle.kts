2be68f1c66b0ec85ddd582a1af33e9659e95b89d
Walter-Henri <walterhenri0@gmail.com>
2026-02-21 13:36:13 -0300

    Fix: Add root build.gradle.kts to coordinate plugin versions

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+// Root build file
+plugins {
+    alias(libs.plugins.com.android.application) apply false
+    alias(libs.plugins.com.android.library) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.plugin.compose) apply false
+    alias(libs.plugins.com.chaquo.python) apply false
+    alias(libs.plugins.com.google.dagger.hilt.android) apply false
+    alias(libs.plugins.com.google.devtools.ksp) apply false
+}
